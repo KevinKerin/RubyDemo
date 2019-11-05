4149b374a0f13e87492732ff48ca3528809e5b32
Ubuntu <ubuntu@ip-172-31-88-101.ec2.internal>
2019-11-05 17:58:54 +0000

    Fixed pageception

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     
 get 'pages/page1', to: 'pages#page1'
-get 'pages/pageception1', to: 'pages#pageception1'
+get 'pages/pageception/pageception1', to: 'pages#pageception#pageception1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
